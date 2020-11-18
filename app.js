1e0cff672087afb568540e56e2a2b33983737e4d
Jmwofford <maurice.waffles@gmail.com>
2020-11-18 10:33:44 -0600

    initialCommit2:

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ const showForm2=()=>{
     $('.formBox').append("<form><div class='form-group'><label for='exampleInputEmail1'>Email address</label><input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'><small id='emailHelp' class='form-text text-muted'>We'll never share your email with anyone else.</small></div><div class='form-group'><label for='exampleInputPassword1'>Resources Needed</label><input type='password' class='form-control' id='exampleInputPassword1'></div><div class='form-group form-check'><input type='checkbox' class='form-check-input' id='exampleCheck1'><label class='form-check-label' for='exampleCheck1'>Send Me Resources</label></div><button type='submit' class='btn btn-primary'>Submit</button></form>").prepend("<img class='formImg' src='https://media.ford.com/content/fordmedia/fna/us/en/news/2016/01/18/ford-extends-commitment-to-african-american-community-with-forum.img.png/1453385717638.jpg'>")
 }
 const toggleMusic = () =>{
-    $('.bodyA').play("<iframe width='5px' height='5px' src='https://www.youtube.com/embed/25Xrf4oyG3g' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+    $('body').append("<iframe width='5px' height='5px' src='https://www.youtube.com/embed/25Xrf4oyG3g' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+}
+const addIframe= (iframe)=>{
+    
 }
