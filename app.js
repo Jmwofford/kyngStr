d74281a506019d232934b218151160d6690734af
Jmwofford <maurice.waffles@gmail.com>
2020-11-17 23:02:13 -0600

    initialCommit2:

--- app.js
+++ app.js
@@ -9,3 +9,4 @@ const showForm=()=>{
 const showForm2=()=>{
     $('.formBox').append("<form><div class='form-group'><label for='exampleInputEmail1'>Email address</label><input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'><small id='emailHelp' class='form-text text-muted'>We'll never share your email with anyone else.</small></div><div class='form-group'><label for='exampleInputPassword1'>Resources Needed</label><input type='password' class='form-control' id='exampleInputPassword1'></div><div class='form-group form-check'><input type='checkbox' class='form-check-input' id='exampleCheck1'><label class='form-check-label' for='exampleCheck1'>Send Me Resources</label></div><button type='submit' class='btn btn-primary'>Submit</button></form>").prepend("<img class='formImg' src='https://media.ford.com/content/fordmedia/fna/us/en/news/2016/01/18/ford-extends-commitment-to-african-american-community-with-forum.img.png/1453385717638.jpg'>")
 }
+
